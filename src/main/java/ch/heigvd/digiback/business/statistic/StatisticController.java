2047bab0ef7089c1c5457cda1bb276c6b070a532
ClarFleu <clarisse.fleu@gmail.com>
2021-06-26 22:15:44 +0200

    Change to autowired

--- src/main/java/ch/heigvd/digiback/business/statistic/StatisticController.java
+++ src/main/java/ch/heigvd/digiback/business/statistic/StatisticController.java
@@ -2,18 +2,17 @@ package ch.heigvd.digiback.business.statistic;
 
 import ch.heigvd.digiback.business.movement.MovementRepository;
 import ch.heigvd.digiback.business.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/stat")
 public class StatisticController {
-
+    @Autowired
     private UserRepository userRepository;
+
+    @Autowired
     private MovementRepository movementRepository;
 
-    public StatisticController(UserRepository userRepository, MovementRepository movementRepository) {
-        this.userRepository = userRepository;
-        this.movementRepository = movementRepository;
-    }
 }
